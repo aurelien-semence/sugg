db55e2cdd0dd1d577a070052d03fcc7365585379
Aurélien SEMENCE <asemence@suggpro.com>
2021-07-05 10:22:11 +0200

    Fix code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
@@ -7,122 +5,108 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import React, { useState } from 'react';
-import { Redirect, Route } from "react-router-dom";
-
-
+import React, {useState} from 'react';
 
 const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
 }));
 
 
-
-
+const validEmail = 'test@admin.com'
+const validPassword = 'TEST'
 
 function App() {
+    const classes = useStyles();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-  const history = useHistory()
+    const handleSignIn = (password: string, email: string) => {
+        if (email === validEmail && password === validPassword) {
+            window.location.href = "https://google.com"
+        }
+    }
 
-  const myFunction = (yourPassword:String,yourEmail:String,email:String,password:String) => {
+    return (
+        <div className="App">
+            <Container component="main" maxWidth="xs">
+                <CssBaseline/>
+                <div className={classes.paper}>
 
-      if(yourEmail === email && yourPassword === password)    
-        return( window.location.href =    "https://google.com")
-  }
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <form className={classes.form}>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                            onChange={event => setEmail(event.target.value)}
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            onChange={event => setPassword(event.target.value)}
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary"/>}
+                            label="Remember me"
+                        />
+                        <Button
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                            onClick={() => handleSignIn(password, email)}
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container>
 
-  
-  
-   
- 
+                            <Grid item>
+                                <Link href="#" variant="body2">
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </div>
 
-  const classes = useStyles();
-  const [email, setEmail] = useState('test@admin.com');
-  const [password, setPassword] = useState('TEST');
-  const [yourEmail, setYourEmail] = useState('');
-  const [yourPassword, setYourPassword] = useState('');
+            </Container>
+        </div>
 
-  return (
-    <div className="App">
-       <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-       
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <form className={classes.form}>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-            onChange={event => setYourEmail(event.target.value)}
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            onChange={event => setYourPassword(event.target.value)}
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-            onClick={event => myFunction(yourPassword,yourEmail,email,password)}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-      
-    </Container>
-    </div>
-
-  );
+    );
 }
 
 export default App;
